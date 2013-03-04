5c2f3e6db926bf59a6f99e02ab697f5af629a0c1
Mizan Rahman <mizan.cse.buet@gmail.com>
2013-03-05 00:55:59 +0600

    initial commit with imported project

---
+++ WcfServiceConsumer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WcfServiceProxy;
+
+namespace WcfServiceConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Proxy serviceProxy = new Proxy();
+            string data = serviceProxy.getData(0);
+        }
+    }
+}
---
+++ WcfServiceProxy/Proxy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WcfServiceProxy.WcfServiceReference;
+
+namespace WcfServiceProxy
+{
+    public class Proxy
+    {
+        public string getData(int arg)
+        {
+            Service1Client client = new Service1Client();
+            return client.GetData(arg);
+
+        }
+    }
+}
